807039c3a0f792ab050e1f42b99048b0cc99771f
Warona Lekoba <wlekoba@yahoo.com>
2022-07-09 15:59:44 +0200

    Final Commit

---
+++ FlyAway/src/main/java/com/database/MySQLConnection.java
@@ -0,0 +1,53 @@
+package com.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+
+	public static Connection getConnection()
+	{
+		
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/flyawaydb","root","9193576431Wb");
+			System.out.println("DB Connected");
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return con;
+	}
+	public static Connection getConnection(String url, String driver, String username, String password)
+	{
+		
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			System.out.println("Driver loaded");
+			// mysql on windows : 3306
+			con = DriverManager.getConnection(url, username, password);
+					
+			System.out.println("DB Connected with parameters from the context");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	
+
+}
+
---
+++ FlyAway/src/main/java/com/servlets/BookStepThreeServlet.java
@@ -0,0 +1,63 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.database.AirlinesDB;
+import com.database.FlightsDB;
+import com.database.PlacesDB;
+
+/**
+ * Servlet implementation class bookStepThree
+ */
+@WebServlet("/bookStepThree")
+public class BookStepThreeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BookStepThreeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		FlightsDB flightsdb = new FlightsDB();
+		PlacesDB placesdb = new PlacesDB();
+		AirlinesDB airlinesdb = new AirlinesDB();
+		RequestDispatcher dispatcher = request.getRequestDispatcher("bookStepThree.jsp");
+		String flightIdString = request.getParameter("flightId");
+		int flightId = Integer.parseInt(flightIdString);
+		HttpSession session = request.getSession();
+		int passengers = (Integer) session.getAttribute("passengers");
+		//session.setAttribute("passengers", passengers);
+		session.setAttribute("sourceName", placesdb.getPlaceById(flightsdb.getFlightById(flightId).getSourceId()).getPname());
+		session.setAttribute("destinationName", placesdb.getPlaceById(flightsdb.getFlightById(flightId).getDestinationId()).getPname());
+		session.setAttribute("airline", airlinesdb.getAirlineById(flightsdb.getFlightById(flightId).getAirlineId()).getAname());
+		session.setAttribute("priceforone", flightsdb.getFlightById(flightId).getPrice());
+		session.setAttribute("priceforall", flightsdb.getFlightById(flightId).getPrice()*passengers);
+		session.setAttribute("flightid",flightId);
+		dispatcher.forward(request, response);
+	}
+
+}
+
